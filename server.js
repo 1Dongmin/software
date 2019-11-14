db5db8d350fedf95baf653ae27d875ffb2effa08
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2019-11-14 19:27:25 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -9,28 +13,17 @@ function onSignIn(googleUser) {
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
-http.createServer((request, response) => {
-  const path = url.parse(request.url, true).pathname; // url에서 path 추출
-  if (request.method === 'GET') { // GET 요청이면
-    if (path === '/about') { // 주소가 /about이면
-      response.writeHead(200,{'Content-Type':'text/html'}); // header 설정
-      fs.readFile(__dirname + '/about.html', (err, data) => { // 파일 읽는 메소드
-        if (err) {
-          return console.error(err); // 에러 발생시 에러 기록하고 종료
-        }
-        response.end(data, 'utf-8'); // 브라우저로 전송
-      });
-    } else if (path === '/') { // 주소가 /이면
-      response.writeHead(200,{'Content-Type':'text/html'});
-      fs.readFile(__dirname + '/main.html', (err, data) => {
-        if (err) {
-          return console.error(err);
-        }
-        response.end(data, 'utf-8');
-      });
-    } else { // 매칭되는 주소가 없으면
-      response.statusCode = 404; // 404 상태 코드
-      response.end('주소가 없습니다');
-    }
-  }
-}).listen(8080);
+app.use(express.static(path.join(__dirname, 'html')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'main.html'));
+});
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'about.html'));
+});
+app.listen(8080, () => {
+  console.log('Express App on port 8080!');
+});
+app.use((req, res, next) => {
+  console.log('안녕!');
+  next();
+})
